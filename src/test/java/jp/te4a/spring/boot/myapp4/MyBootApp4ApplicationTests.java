6c9f3ce4c573bd7823b3ff84fde161e796ea77b2
西坂 瞬 <180581@jc-21.jp>
2021-09-27 12:26:28 +0900

    iroiro

--- src/test/java/jp/te4a/spring/boot/myapp4/MyBootApp4ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp4/MyBootApp4ApplicationTests.java
@@ -1,5 +1,7 @@
 package jp.te4a.spring.boot.myapp4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class MyBootApp4ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		HelloController helloController = new HelloController();
+		String expect  ="this is Spring Boot sample.";
+		String actual = helloController.index();
+		assertEquals(expect, actual);
+
 	}
 
 }
