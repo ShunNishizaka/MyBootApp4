3dee0cbf3b1d8b2107905c5fab67609fa6591147
Shun <83932733+ShunNishizaka@users.noreply.github.com>
2021-09-30 13:57:11 +0900

    Update MyBootApp5ApplicationTests.java

--- src/test/java/jp/te4a/spring/boot/myapp5/MyBootApp5ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp5/MyBootApp5ApplicationTests.java
@@ -1,14 +1,13 @@
-
 package jp.te4a.spring.boot.myapp5;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class MyBootApp5Application {
+@SpringBootTest
+class MyBootApp5ApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyBootApp5Application.class, args);
+    @Test
+    void contextLoads() {
     }
 
 }
